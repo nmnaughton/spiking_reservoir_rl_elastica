b5b3b009d0e76d2b3e1c0761d46508b7cb4f339f
kshivvy <keshavshivam18@gmail.com>
2020-09-26 00:31:02 -0500

    Adding .sh scripts

---
+++ run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+#PBS -l nodes=32:ppn=32:xe 
+#PBS -l walltime=20:00:00 
+#PBS -q normal 
+#PBS -N testjob 
+#PBS -e $PBS_JOBNAME$PBS_JOBID.err 
+#PBS -o $PBS_JOBNAME$PBS_JOBID.out 
+#PBS -m abe 
+#PBS -M kshivam2@illinois.edu 
+#PBS -l gres=shifter
+
+SCRIPTBASEPATH=$PBS_O_WORKDIR 
+SCRIPTNAME=$0 
+PROGNAME="reservoir_rl" 
+NTHREADS=32 NNODES=32
+
+##########################################
+#                                        #
+#   Output some useful job information.  #
+#                                        #
+##########################################
+
+# echo ------------------------------------------------------ 
+# echo -n 'Job is running on node '; cat $PBS_NODEFILE 
+# echo ------------------------------------------------------ 
+# echo PBS: qsub is running on $PBS_O_HOST 
+# echo PBS: originating queue is $PBS_O_QUEUE 
+# echo PBS: executing queue is $PBS_QUEUE 
+# echo PBS: working directory is $PBS_O_WORKDIR 
+# echo PBS: execution mode is $PBS_ENVIRONMENT 
+# echo PBS: job identifier is $PBS_JOBID 
+# echo PBS: job name is $PBS_JOBNAME 
+# echo PBS: node file is $PBS_NODEFILE 
+# echo PBS: current home directory is $PBS_O_HOME 
+# echo PBS: PATH = $PBS_O_PATH 
+# echo ------------------------------------------------------ 
+
+# Load shifter module and set IMG
+module load shifter 
+IMG=docker:kshiv/blue_waters:latest
+
+# Run containerized image
+aprun -b -N 1 shifter --image=$IMG
